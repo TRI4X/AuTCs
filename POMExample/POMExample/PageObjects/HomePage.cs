7cf8c5f11d9c898010571f2b934809e403f29aa2
TRI4X <32551666+TRI4X@users.noreply.github.com>
2017-10-05 20:01:22 +0200

    first try
    some TC has been created but nedd to be modify more properly

---
+++ POMExample/POMExample/PageObjects/HomePage.cs
@@ -0,0 +1,31 @@
+﻿
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using OnlineStore.TestCases;
+
+namespace OnlineStore.PageObjects
+{
+    class HomePage
+    {
+        private IWebDriver driver;
+
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public static Boolean ClickAndActive(IWebDriver driver)
+        {
+            driver.FindElement(By.Id("home")).Click();
+            System.Threading.Thread.Sleep(1000);//need some sleep due to when click method has been used, website temporarliy unavailable (ms)
+            IWebElement parent = driver.FindElement(By.Id("home")).FindElement(By.XPath(".."));
+            return testcases.elementHasClass(parent, "active");
+        }
+
+       
+       
+    }
+}
